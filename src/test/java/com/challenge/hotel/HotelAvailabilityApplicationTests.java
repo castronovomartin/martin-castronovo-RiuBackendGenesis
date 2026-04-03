f9f9ab1ba87cbaed3399cd4080c1a8b9dfb864e9
Tincho Castronovo <martin.castronovo@astropay.com>
2026-04-03 12:02:04 -0300

    chore: initialize project with base structure and stable dependencies

---
+++ src/test/java/com/challenge/hotel/HotelAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAvailabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
